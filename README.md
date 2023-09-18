e8388b440aaf2f71481866e14dc26f8105a46f2e
Tomas Bazan <tomas.pablo.bazan@mi.unc.edu.ar>
2023-09-17 21:30:44 -0300

    style: change format with Prettier and ESLint

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Ing_Soft2023_LaCosa_LCSP
 Desarrollo mediante React del frontend  para implementar el juego "La Cosa". Grupo "Las Chicas Super Poderosas"
+
+Before commits run 
+``` npm run format
+``` npm run lint
--- front/src/App.js
+++ front/src/App.js
@@ -1,47 +1,47 @@
-import logo from "./logo.svg";
-import "./styles/App.css";
-import Button from "@mui/material/Button";
-import { useEffect, useState } from "react";
+import logo from './logo.svg';
+import './styles/App.css';
+import Button from '@mui/material/Button';
+import {useEffect, useState} from 'react';
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
+	const [counter, setCounter] = useState(0);
 
-  const handleClick = () => {
-    let newCounter = counter + 1;
-    setCounter(counter + 1);
-  };
+	const handleClick = () => {
+		const newCounter = counter + 1;
+		setCounter(newCounter + 1);
+	};
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCounter(0);
-    }, 5000);
-    return () => clearInterval(interval);
-  });
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setCounter(0);
+		}, 5000);
+		return () => clearInterval(interval);
+	});
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <p>The counter is: {counter}</p>
-      <body className="boton">
-        <Button variant="text" onClick={handleClick}>
-          First Button
-        </Button>
-      </body>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className='App-link'
+					href='https://reactjs.org'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					Learn React
+				</a>
+			</header>
+			<p>The counter is: {counter}</p>
+			<body className='boton'>
+				<Button variant='text' onClick={handleClick}>
+					First Button
+				</Button>
+			</body>
+		</div>
+	);
 };
 
 export default App;
