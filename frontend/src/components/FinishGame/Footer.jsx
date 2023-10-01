02af6c40ef65c4d4d19acc496117ccf9977d0354
Tomas Bazan <tomas.pablo.bazan@mi.unc.edu.ar>
2023-10-01 20:07:16 -0300

    refactor: deleting unecesarry function

--- frontend/src/components/FinishGame/Footer.jsx
+++ frontend/src/components/FinishGame/Footer.jsx
@@ -2,17 +2,11 @@
 import React from 'react';
 import {CardFooter, Button} from '@chakra-ui/react';
 import {Link} from 'react-router-dom';
-
 export const GoHome = () => {
-	const returnToHome = () => {
-		window.location.href = '/';
-	};
 	return (
 		<CardFooter>
 			<Link to='/'>
-				<Button colorScheme='blackAlpha' onClick={returnToHome}>
-					Play Again
-				</Button>
+				<Button colorScheme='blackAlpha'>Play Again</Button>
 			</Link>
 			{/* Should have the Button to go to the home page */}
 		</CardFooter>
