652675c187e5910e4702fad499bfead31c750a77
Tomas Bazan <tomas.pablo.bazan@mi.unc.edu.ar>
2023-10-01 20:04:32 -0300

    refactor: make not necesary login again

--- frontend/src/appActions.js
+++ frontend/src/appActions.js
@@ -3,3 +3,4 @@ import {createAction} from '@reduxjs/toolkit';
 // Definiciones de acciones
 export const setPlayerName = createAction('player/setName');
 export const setPlayerId = createAction('player/setId');
+export const setPlayerLogedIn = createAction('player/setLogedIn');
