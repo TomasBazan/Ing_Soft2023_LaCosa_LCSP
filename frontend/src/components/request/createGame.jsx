dd2470aaa8e685e2cd769f67272db2fbf2ff6598
Mili Carabelos <mili.carabelos03@gmail.com>
2023-10-01 10:21:45 -0300

    feat Adding the http request for CreateGameForm

--- frontend/src/components/request/createGame.jsx
+++ frontend/src/components/request/createGame.jsx
@@ -1,24 +1,42 @@
-const SERVER_URL = 'https://localhost:8000/game';
+const SERVER_URL = 'http://localhost:8000/game';
 
-const createGame = async ({idPlayer, game}) => {
+export const CreateGame = async ({game}) => {
 	const parseJSONResponse = (response) => {
 		return new Promise((resolve) =>
-			response.json().then((json) =>
-				resolve({
-					status: response.status,
-					ok: response.ok,
-					json,
-				}),
-			),
+			response.json().then((json) => {
+				if (json.ok) {
+					resolve({
+						status: response.status,
+						ok: response.ok,
+						detail: response.detail,
+						gameId: response.data.gameId,
+					});
+				} else {
+					resolve({
+						status: response.status,
+						ok: response.ok,
+						detail: response.detail,
+					});
+				}
+			}),
 		);
 	};
+
+	/* const gameToSend = {
+		id_player: game.id_player,
+		name: game.name,
+		min_players: game.min_players,
+		max_players: game.max_players,
+		password: game.password,
+	}; */
 	const config = {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
 		},
-		body: JSON.stringify({idPlayer, game}),
+		body: JSON.stringify(game),
 	};
+	console.log('game to seeeend', {game});
 	return new Promise((resolve, reject) => {
 		fetch(SERVER_URL, config)
 			.then(parseJSONResponse)
@@ -33,4 +51,4 @@ const createGame = async ({idPlayer, game}) => {
 			});
 	});
 };
-export default createGame;
+//export default CreateGame;
