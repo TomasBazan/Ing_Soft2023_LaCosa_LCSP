ad3fb7ee6459d10db8a3052671e6bf916df48c4a
klartz <kurtzlara00@gmail.com>
2023-10-02 09:00:40 -0300

    refactor: perform http request using modularized functions. Remove mocked data

--- frontend/src/components/Deck/Deck.jsx
+++ frontend/src/components/Deck/Deck.jsx
@@ -1,42 +1,29 @@
 import Card from '../Card/Card.jsx';
+import getCard from '../request/getCard';
 import {useState} from 'react';
 import {useDispatch} from 'react-redux';
 import {appendToHand} from '../../services/handSlice';
 
 const Deck = () => {
 	const backImage = 'img65';
+	const userId = 1;
 
 	const [clicked, setClicked] = useState(false);
 	const [imageSrc, setImageSrc] = useState(backImage);
 	const dispatch = useDispatch();
 
-	const mockPick = JSON.parse(
-		JSON.stringify({
-			status: '',
-			message: '',
-			data: ['img197'],
-		}),
-	);
-
 	// when deck is clicked
-	const handleClick = () => {
-		// try {
-		// 	// fetches the next card in deck
-		// 	const response = await fetch('http://localhost:8000/hand/', {
-		// 		method: 'PUT',
-		// 	});
-		// 	setImageSrc(response.json().data);
-		// } catch (error) {
-		// 	console.error("Error fetching player's hand:", error);
-		// }
-
+	const handleClick = async () => {
 		// if it wasn't clicked already
 		if (!clicked) {
-			// display first hand
-			setImageSrc(mockPick.data[0]);
+			const res = await getCard(userId);
+			const pickedCard = res.json.data;
+
+			// display first card
+			setImageSrc(pickedCard[0]);
 			// wait and update player's hand with picked card
 			setTimeout(() => {
-				dispatch(appendToHand(mockPick.data));
+				dispatch(appendToHand(pickedCard));
 				// display back of next card in deck
 				setImageSrc(backImage);
 			}, 1000);
