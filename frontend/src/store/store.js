ee94cc1bea25ba5e26026ef9e5d0cc61a2028ab0
klartz <kurtzlara00@gmail.com>
2023-10-02 10:37:47 -0300

    feat: implement redux state management for the PlayArea component

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,13 +1,16 @@
 import {configureStore} from '@reduxjs/toolkit';
 import playerReducer from '../playerSlice.js';
 import handReducer from '../services/handSlice.js';
+import playAreaSlice from '../services/playAreaSlice.js';
 
 // configuracion de los estados y sus reducers
 const store = configureStore({
 	reducer: {
 		player: playerReducer,
 		hand: handReducer,
+		playArea: playAreaSlice,
 	},
+	devTools: true,
 });
 
 export default store;
