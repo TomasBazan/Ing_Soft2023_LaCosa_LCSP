3a7ae7bf2ff046f9cb6070f1df4d66ca1845c463
Mili Carabelos <mili.carabelos03@gmail.com>
2023-10-02 19:30:08 -0300

    feat lobby component

--- frontend/src/appActions.js
+++ frontend/src/appActions.js
@@ -5,3 +5,6 @@ export const setPlayerName = createAction('player/setName');
 export const setPlayerId = createAction('player/setId');
 export const setHand = createAction('hand/setHand');
 export const appendToHand = createAction('hand/appendToHand');
+export const setLobby = createAction('game/setLobby');
+export const appendToLobby = createAction('game/appendToLobby');
+export const setCanStart = createAction('game/setCanStart');
