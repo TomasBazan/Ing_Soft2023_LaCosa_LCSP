e7ab482a278afd5622f08ce3a07ad33668bb3790
klartz <kurtzlara00@gmail.com>
2023-10-02 08:47:19 -0300

    refactor: perform http request with modularized function

--- frontend/src/components/Hand/Hand.jsx
+++ frontend/src/components/Hand/Hand.jsx
@@ -1,5 +1,6 @@
 import './Hand.css';
 import Card from '../../components/Card/Card.jsx';
+import getHand from '../request/getHand';
 
 import {useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
@@ -7,6 +8,8 @@ import {setHand} from '../../services/handSlice';
 
 // represents a player's hand
 const Hand = () => {
+	const userId = 1;
+
 	// select cards state
 	const cards = useSelector((state) => state.hand.cards);
 	const dispatch = useDispatch();
@@ -15,19 +18,8 @@ const Hand = () => {
 	useEffect(() => {
 		// fetch  player's hand
 		const fetchHand = async () => {
-			try {
-				const response = await fetch('/hand', {
-					method: 'GET',
-					headers: {
-						'Content-Type': 'application/json',
-					},
-				});
-				const res = await response.json();
-				// set player's hand state using redux
-				dispatch(setHand(res.data));
-			} catch (error) {
-				console.error("Error fetching player's hand:", error);
-			}
+			const res = await getHand(userId);
+			dispatch(setHand(res.json.data));
 		};
 		fetchHand();
 	}, [dispatch]);
