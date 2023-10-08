0aa0aea65cc3baddc90d9e7332e4db45300df5ec
klartz <kurtzlara00@gmail.com>
2023-10-08 13:19:20 -0300

    refactor: set initial card back for deck when component mounts. Update backImage to include back image for cards of type panic

--- frontend/src/components/Deck/Deck.jsx
+++ frontend/src/components/Deck/Deck.jsx
@@ -1,16 +1,25 @@
 import Card from '../Card/Card.jsx';
 import getCard from '../request/getCard';
-import {useState} from 'react';
+// eslint-disable-next-line no-unused-vars
+import React, {useState, useEffect} from 'react';
 import {useDispatch} from 'react-redux';
 import {appendToHand} from '../../services/handSlice';
 
 const Deck = () => {
-	const backImage = 'img65';
+	const backImage = ['panic-reverse.jpg', 'reverse.jpg'];
+
+	// userId del jugador en turno, deberíamos obtenerlo del estado de la partida
 	const userId = 1;
 
 	const [clicked, setClicked] = useState(false);
 	const [imageSrc, setImageSrc] = useState(backImage);
 	const dispatch = useDispatch();
+
+	useEffect(() => {
+		// deberíamos obtener el tipo de la primera carta cuando empieza la partida
+		setImageSrc(backImage[1]);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
 	// when deck is clicked
 	const handleClick = async () => {
