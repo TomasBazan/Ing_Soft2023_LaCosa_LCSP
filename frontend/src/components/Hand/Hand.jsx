1be32ce5d63aedb346bac41db0757b2748588201
klartz <kurtzlara00@gmail.com>
2023-10-08 13:15:26 -0300

    feat: implement the play card action

--- frontend/src/components/Hand/Hand.jsx
+++ frontend/src/components/Hand/Hand.jsx
@@ -1,16 +1,24 @@
 import './Hand.css';
 import Card from '../../components/Card/Card.jsx';
 import getHand from '../request/getHand';
+import playCard from '../request/playCard';
 
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import {setHand} from '../../services/handSlice';
+import {
+	setHand,
+	removeFromHand,
+	addToPlayArea,
+	cleanPlayArea,
+} from '../../appActions';
 import {v4 as uuidv4} from 'uuid';
 
 // represents a player's hand
 const Hand = () => {
 	const userId = 1;
+	const targetId = 2;
 
+	const [selectedCard, setSelectedCard] = useState(null);
 	// select cards state
 	const cards = useSelector((state) => state.hand.cards);
 	const dispatch = useDispatch();
@@ -25,11 +33,36 @@ const Hand = () => {
 		fetchHand();
 	}, [dispatch]);
 
+	/*
+		When cards are clicked once, they get selected. If clicked again, they are played.
+		Playing a card consists of removing it from the player's hand and adding it to the play area.
+		Play area gets cleaned after 1 second.
+	*/
+	const handleClick = async (clickedCard) => {
+		if (selectedCard === clickedCard) {
+			const cardToken = String(clickedCard);
+			// eslint-disable-next-line no-unused-vars
+			const res = await playCard({cardToken, userId, targetId});
+
+			// resolver efectos de la jugada sobre la partida
+
+			dispatch(removeFromHand(clickedCard));
+			dispatch(addToPlayArea(clickedCard));
+			setTimeout(() => dispatch(cleanPlayArea()), 1000);
+		} else {
+			setSelectedCard(clickedCard);
+		}
+	};
 	// render cards in hand side by side
 	return (
 		<div className='hand'>
 			{cards.map((card) => (
-				<Card key={uuidv4()} token={card} />
+				<Card
+					className={`card ${selectedCard === card ? 'selected' : ''}`}
+					key={uuidv4()}
+					onClick={() => handleClick(card)}
+					token={card}
+				/>
 			))}
 		</div>
 	);
