3866a0feda062bc44bd7b416354abe2dd25e0ed3
Tomas Bazan <tomas.pablo.bazan@mi.unc.edu.ar>
2023-10-16 09:22:19 -0300

    Test: adding mock to allow run the component

---
+++ frontend/src/mocks/gameHandlers.js
@@ -0,0 +1,33 @@
+import {rest} from 'msw';
+
+export const gameHandlers = [
+	rest.get('http://localhost:8000/game', (req, res, ctx) => {
+		console.log('getGameStatus:', req);
+		return res(
+			ctx.status(200),
+			ctx.json({
+				status: 200,
+				detail: 'Some detail mocked',
+				data: {
+					my_position: 0,
+					my_rol: 0,
+					current_player: 6,
+					players: [
+						{name: 'tomas', id: 1, position: 0, is_alive: true},
+						{name: 'juan', id: 2, position: 1, is_alive: true},
+						{name: 'pedro', id: 3, position: 2, is_alive: true},
+						{name: 'pepe', id: 4, position: 3, is_alive: true},
+						{name: 'mili', id: 5, position: 4, is_alive: true},
+						{name: 'lara', id: 6, position: 5, is_alive: true},
+						{name: 'lauti', id: 7, position: 6, is_alive: true},
+						{name: 'nico', id: 8, position: 7, is_alive: true},
+						{name: 'diego', id: 9, position: 8, is_alive: true},
+						{name: 'laura', id: 10, position: 9, is_alive: true},
+						{name: 'santi', id: 11, position: 10, is_alive: true},
+						{name: 'chun', id: 12, position: 11, is_alive: false},
+					],
+				},
+			}),
+		);
+	}),
+];
