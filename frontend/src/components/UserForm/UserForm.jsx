cb9ef1f95120dd9804b79eadb92ce09fc27ee813
Tomas Bazan <tomas.pablo.bazan@mi.unc.edu.ar>
2023-10-19 19:25:24 -0300

    reafactor: change status of id in UserForm

--- frontend/src/components/UserForm/UserForm.jsx
+++ frontend/src/components/UserForm/UserForm.jsx
@@ -1,6 +1,4 @@
 import {useFormik} from 'formik';
-import {useDispatch, useSelector} from 'react-redux';
-import {setPlayerId, setPlayerName, setPlayerLogedIn} from '../../appActions';
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import {Link} from 'react-router-dom';
@@ -14,25 +12,20 @@ import {
 	Button,
 	HStack,
 } from '@chakra-ui/react';
-// import SendPlayerName from '../request/sendPlayerName.mock';
 
 // UserForm is our functional component
 const UserForm = () => {
-	const dispatch = useDispatch();
-	const firstPlayer = useSelector((state) => state.player);
+	const firstPlayer = sessionStorage.getItem('player');
 	const initialValues = {username: ''};
 
 	const onSubmit = async (values) => {
 		const actualPlayer = {name: values.username, id: 0};
 		try {
 			const resp = await sendPlayerName({player: actualPlayer});
-			const updatedPlayer = {name: resp.name, id: resp.id};
-			dispatch(setPlayerId(updatedPlayer.id));
-			dispatch(setPlayerName(updatedPlayer.name));
-			dispatch(setPlayerLogedIn(true));
+			const updatedPlayer = {name: resp.name, id: resp.id, isLoged: true};
+			sessionStorage.setItem('player', JSON.stringify(updatedPlayer));
 			alert('Succes: ' + resp.detail);
 		} catch (error) {
-			// The status code is missing in the response
 			console.error('Error: ' + error);
 			if (!error.ok) {
 				alert(error.detail);
@@ -54,47 +47,87 @@ const UserForm = () => {
 		onSubmit,
 		validate,
 	});
+	// if the firstPlayer exists prin the isLoged field
+	let flag = false;
+	if (firstPlayer) {
+		flag = JSON.parse(sessionStorage.getItem('player')).isLoged;
+	} else {
+		flag = false;
+	}
+
 	return (
-		<Center height='100vh'>
-			<Box width='25%' textAlign='center' p={5}>
-				<form onSubmit={formik.handleSubmit}>
-					{!firstPlayer.loged ? (
-						<FormControl>
-							<Text fontSize='3xl' fontWeight='bold'>
-								Elige tu nickname
-							</Text>
-							<Input
-								type='text'
-								id='username'
-								name='username'
-								onChange={formik.handleChange}
-								onBlur={formik.handleBlur}
-								value={formik.values.username}
-							/>
-							{formik.errors.username ? (
-								<div className='error'>{formik.errors.username}</div>
-							) : null}
-							<Button
-								colorScheme='gray'
-								type='submit'
-								onClick={formik.handleSubmit}
-							>
-								Submit
-							</Button>
-						</FormControl>
-					) : (
-						<HStack spacing={4}>
-							<Link to='Games/'>
-								<Button>Unirse a partida</Button>
-							</Link>
-							<Link to='/CreateGame'>
-								<Button>Crear nueva partida</Button>
-							</Link>
-						</HStack>
-					)}
-				</form>
+		<Box
+			height='100vh'
+			// bgGradient='linear(to-r, gray.900,black,gray.900)'
+		>
+			<Box
+				bgImage='url(http://localhost:5173/src/assets/LaCosaBackground.png)'
+				position='relative'
+				bgSize='center'
+				bgPosition='center'
+				bgAttachment='fixed'
+				bgRepeat='no-repeat' // Set to 'no-repeat' to make it appear only once
+				height='100%'
+			>
+				<Center height='100%' color='white'>
+					<Box
+						width='25%'
+						textAlign='center'
+						p={5}
+						bg='transparent'
+						rounded='lg'
+					>
+						<form onSubmit={formik.handleSubmit}>
+							{!flag ? (
+								<FormControl>
+									<Text
+										fontSize='3xl'
+										fontWeight='bold'
+										color='white'
+										opacity='0.8'
+									>
+										Elige tu nickname
+									</Text>
+									<Input
+										type='text'
+										id='username'
+										name='username'
+										onChange={formik.handleChange}
+										onBlur={formik.handleBlur}
+										value={formik.values.username}
+									/>
+									{formik.errors.username ? (
+										<div className='error'>{formik.errors.username}</div>
+									) : null}
+									<Button
+										colorScheme='transparent'
+										variant='outline'
+										type='submit'
+										onClick={formik.handleSubmit}
+									>
+										Submit
+									</Button>
+								</FormControl>
+							) : (
+								<HStack spacing={4}>
+									<Link to='/Games/'>
+										<Button variant='outline' colorScheme='transparent'>
+											Unirse a partida
+										</Button>
+									</Link>
+									<Link to='/CreateGame'>
+										<Button variant='outline' colorScheme='transparent'>
+											Crear nueva partida
+										</Button>
+									</Link>
+								</HStack>
+							)}
+						</form>
+					</Box>
+				</Center>
 			</Box>
-		</Center>
+		</Box>
 	);
 };
+
 export default UserForm; // Export UserForm as the default export
