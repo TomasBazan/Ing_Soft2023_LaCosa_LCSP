ecf61504150512b6c71f8ddac7306092eefb400a
klartz <kurtzlara00@gmail.com>
2023-10-23 13:19:00 -0300

    feat: make http request to discard card

--- frontend/src/components/DiscardPile/DiscardPile.jsx
+++ frontend/src/components/DiscardPile/DiscardPile.jsx
@@ -3,10 +3,12 @@ import {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {addToDiscardPile, removeFromHand} from '../../appActions';
 import Card from '../Card/Card.jsx';
+import discardCard from '../request/discardCard';
 
 const DiscardPile = () => {
 	const selectedCard = useSelector((state) => state.hand.selectedCard);
 	const discardedCard = useSelector((state) => state.discardPile.discardedCard);
+	const idPlayer = JSON.parse(sessionStorage.getItem('player')).id;
 	const [highlight, setHighlight] = useState(false);
 	const dispatch = useDispatch();
 
@@ -25,13 +27,13 @@ const DiscardPile = () => {
 		When the discard pile is clicked, the selected card is placed in the
 		discard pile and removed from the player's hand.
 	*/
-	const handleClick = () => {
+	const handleClick = async () => {
 		console.log('Click on discard pile');
-		if (selectedCard !== '') {
+		if (selectedCard) {
+			// eslint-disable-next-line no-unused-vars
+			const res = await discardCard({discardedCard: selectedCard, idPlayer});
 			dispatch(addToDiscardPile(selectedCard));
 			dispatch(removeFromHand(selectedCard));
-
-			console.log('Card discarded');
 		}
 	};
 
