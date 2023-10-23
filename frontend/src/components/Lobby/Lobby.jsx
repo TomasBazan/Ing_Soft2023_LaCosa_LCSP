02426d38aa43c7f5a180f4aa46a2300f9ccc73cf
klartz <kurtzlara00@gmail.com>
2023-10-23 15:52:02 -0300

    fix: import

--- frontend/src/components/Lobby/Lobby.jsx
+++ frontend/src/components/Lobby/Lobby.jsx
@@ -1,13 +1,12 @@
 import getLobbyStatus from '../request/getLobbyStatus';
 import {useDispatch, useSelector} from 'react-redux';
-import {setCanStart, setLobby} from '../../appActions';
+import {setCanStart, setLobby, setFirstDeckCardBack} from '../../appActions';
 import {useEffect, useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 import startGame from '../request/startGame';
 import deleteGameJoin from '../request/deleteGameJoin';
 import {VStack, OrderedList, ListItem, Text, Button} from '@chakra-ui/react';
 
-import {setFirstDeckCardBack} from '../appActions';
 // mock respuestas por ahora 		{user_name: 'toomas', id: 5, is_host: 0},
 
 const Lobby = () => {
