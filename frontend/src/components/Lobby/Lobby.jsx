93d8d1326d6f8b4b7eba53f27223668de293f488
klartz <kurtzlara00@gmail.com>
2023-10-23 14:46:30 -0300

    fix: add missing import

--- frontend/src/components/Lobby/Lobby.jsx
+++ frontend/src/components/Lobby/Lobby.jsx
@@ -5,6 +5,7 @@ import {useEffect, useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 import startGame from '../request/startGame';
 import {VStack, OrderedList, ListItem, Text, Button} from '@chakra-ui/react';
+import {setFirstDeckCardBack} from '../appActions';
 
 // mock respuestas por ahora 		{user_name: 'toomas', id: 5, is_host: 0},
 
